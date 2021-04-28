edb1d119b5b3790594ce3d93b460b7780f05459e
Mohammed Sayeed <mdsayeed6220@gmail.com>
2021-04-28 22:45:38 +0530

    add default file

---
+++ default.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    return 0;
+}
